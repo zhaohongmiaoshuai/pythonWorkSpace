48a7e47eca12e831e7237eb4f7ec9da5a0305baa
ZhaoHongMiao <937412729@qq.com>
2019-08-02 23:11:28 +0800

    My first pygame program

---
+++ RequestsTest.py
@@ -0,0 +1,12 @@
+﻿#_*_ coding:UTF-8 _*_
+import requests
+
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36    (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+}
+
+res = requests.get('http://bj.xiaozhu.com/',headers = headers)
+print(res)
+f = open('C:/Users/zhaoo/Desktop/adasd.txt','w+')
+print(res.text)
+f.write(res.text)
