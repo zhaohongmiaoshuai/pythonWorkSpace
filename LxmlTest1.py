01734d36f5471f36df71ce3b935efea818b5c073
ZhaoHongMiao <937412729@qq.com>
2019-08-05 22:35:54 +0800

    My first pygame program

---
+++ LxmlTest1.py
@@ -0,0 +1,13 @@
+﻿#_*_ coding:UTF-8 _*_
+import requests
+from lxml import etree
+
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+}
+
+url = 'http://www.qiushibaike.com/text/'
+res = requests.get(url, headers = headers)
+selector = etree.HTML(res.text)
+id = selector.xpath('//*[@id="qiushi_tag_122086803"]/div[1]/a[2]/h2/text()')[0]
+print(id)
