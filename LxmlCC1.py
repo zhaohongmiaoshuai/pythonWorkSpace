b29066f470dbfb12f3fec9d54c26c2f2c6d9be82
ZhaoHongMiao <937412729@qq.com>
2019-08-06 22:29:49 +0800

    My first pygame program2

---
+++ LxmlCC1.py
@@ -0,0 +1,34 @@
+﻿#_*_ coding:UTF-8 _*_
+from lxml import etree
+import requests
+import csv
+
+fp = open('C:/Users/zhaoo/Desktop/doubanTop250.csv','wt',newline='',encoding='utf-8')
+
+writer = csv.writer(fp)
+writer.writerow(('name', 'url', 'author', 'publisher', 'date', 'price', 'rate', 'comment'))
+
+urls = ['https://book.douban.com/top250?start={}'.format(str(i)) for i in range(0,250,25)]
+
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+}
+
+for url in urls:
+    html = requests.get(url, headers = headers)
+    selector = etree.HTML(html.text)
+    infos = selector.xpath('//tr[@class="item"]')
+    for info in infos:
+        name = info.xpath('td/div/a/@title')[0]
+        url = info.xpath('td/div/a/@href')[0]
+        book_infos = info.xpath('td/p/text()')[0]
+        author = book_infos.split('/')[0]
+        publisher = book_infos.split('/')[-3]
+        date = book_infos.split('/')[-2]
+        price = book_infos.split('/')[-1]
+        rate = info.xpath('td/div/span[2]/text()')[0]
+        comments = info.xpath('td/p/span/text()')
+        comment = comments[0] if len(comments) != 0 else "空"
+        writer.writerow((name, url, author, publisher, date, price, rate, comment))
+
+fp.close()
