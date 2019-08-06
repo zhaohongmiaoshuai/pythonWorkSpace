79f85384e6926fc1ebf8a866a0eb6e6f5e6cb9c4
ZhaoHongMiao <937412729@qq.com>
2019-08-06 22:05:31 +0800

    My first pygame program2

---
+++ CsvTest.py
@@ -0,0 +1,10 @@
+﻿#_*_ coding:UTF-8 _*_
+import csv
+fp = open('C:/Users/zhaoo/Desktop/csvtest.csv','w+',newline='')
+#newline='' 可以解决空行问题
+
+writer = csv.writer(fp)
+writer.writerow(('id', 'name'))
+writer.writerow(('1', 'xiaoming'))
+writer.writerow(('2', 'haha'))
+writer.writerow(('3', 'dsdasdfa'))
